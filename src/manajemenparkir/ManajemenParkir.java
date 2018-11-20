d597827dcbd60d25f34169f1244b54426fae676d
bagas23 <wirayuda87@gmail.com>
2018-11-20 15:17:44 +0700

    Initial commit

---
+++ src/manajemenparkir/ManajemenParkir.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manajemenparkir;
+import UI.LoginUI;
+/**
+ *
+ * @author ASUS
+ */
+public class ManajemenParkir {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginUI A = new LoginUI();
+        A.setVisible(true);
+    }
+    
+}
